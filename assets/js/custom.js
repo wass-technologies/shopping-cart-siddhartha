785f7d03773eafd5ba6bff659cf14fd9bc69eff4
notSinhaSid <deadlysinhasiddhartha2000@gmail.com>
2025-03-31 19:01:31 +0530

    cart display done. qty not working though

---
+++ assets/js/custom.js
@@ -0,0 +1,17 @@
+$(document).ready(function () {
+
+    $('.increment-btn').click(function (e){
+        e.preventDefault();
+
+        var qty =$(this).closest('.card').find('.input-qty').val();
+
+        var value =parseInt(qty, 10);
+        value = isNaN(value)?0: value;
+        if(value<10)
+        {
+            value++;
+            // $('.input-qty').val(value);
+            var qty =$(this).closest('.card').find('.input-qty').val(value);
+        }
+    });
+});
--- includes/addToCart.inc.php
+++ includes/addToCart.inc.php
@@ -3,24 +3,52 @@ require('../includes/db_autoload.inc.php');
 // to check for same product id from both the table
 $sql ="SELECT * FROM addtocart_tb";
 $res1 = $conn->query($sql);
+// initilize the $msg variable so that the each msg content is not overwritten in the loop
+$msg ='';
+
 if($res1->num_rows>0)
 {
     while($row = $res1->fetch_assoc())
     {
         $productId =$row['productId'];
         $productQuantity = $row['productQuantity'];
-        // echo $productQuantity;
-        // echo $productId;
-        $sql2 = "SELECT productName, productPrice FROM productadd_tb WHERE productId ='".$productId."'";
+        
+        $sql2 = "SELECT productName, productPrice, productImage FROM productadd_tb WHERE productId ='".$productId."'";
         $res2 = $conn->query($sql2);
         if($res2->num_rows>0)
         {
-            $row = $res2->fetch_assoc();
-            $productName = $row['productName'];
-            $productPrice = $row['productPrice'];
+            while($row2 = $res2->fetch_assoc())
+            {
+                $productName = $row2['productName'];
+                $productPrice = $row2['productPrice'];
+                $productImage = $row2['productImage'];
 
-            // echo $productName;
-            // echo $productPrice;
+                $msg .= '<div class="card mb-3" style="max-width: 540px;">
+                            <div class="row g-0">
+                                <div class="col-md-4">
+                                <img src="../admin/assets/productImages/'.$productImage.'" class="img-fluid rounded-start mt-2" alt="...">
+                                </div>
+                                <div class="col-md-8">
+                                <div class="card-body">
+                                    <h5 class="card-title">'.$productName.'</h5>
+                                    <p class ="card-text" name="price">Price ₹'.$productPrice.'</p>
+                                    <form action ="" metho="POST">
+                                        <div class="input-group mb-3" style="width:130px">
+                                            <button class="input-group-text decrement-btn">-</button>
+                                            <input type="number" class="form-control bg-white text-center input-qty" disabled value="'.$productQuantity.'">
+                                            <button class="input-group-text increment-btn">+</button>
+                                        </div>
+                                    </form>
+                                </div>
+                                </div>
+                            </div>
+                        </div>';
+            }
+
+        }
+        else
+        {
+            $msg = '<div class="alert alert-warning" role="alert">Nothing in the cart right now</div>';
         }
     }
 }
@@ -37,8 +65,14 @@ if($res1->num_rows>0)
 </head>
 <body>
     <h3>This is show to cart page</h3>
-
-    
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-6">
+                <?php if(isset($msg)){echo $msg;}?>
+            </div>
+        </div>
+    </div>
+<script src="../assets/js/custom.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>    
 </body>
 </html>
